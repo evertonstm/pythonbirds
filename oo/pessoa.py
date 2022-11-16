d4288459978bfd41c9b75dc1e1bda5bce4fef94e
Everton Reis <evertonreis@EvertonReis.local>
2022-11-16 09:56:49 -0400

    Criado atributo de instancia nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,15 @@
 class Pessoa:
-  pass 
+  def __init__(self, nome = None, idade=30):
+    self.idade = idade
+    self.name = nome
+
+  def cumprimentar(self):
+    return f'Olá {id(self)}'
+
+if __name__ == '__main__':
+  p = Pessoa('Simone')
+  print(p.cumprimentar())
+  print(p.name)
+  p.name = 'Everton Reis'
+  print(p.name)
+  print(p.idade)
